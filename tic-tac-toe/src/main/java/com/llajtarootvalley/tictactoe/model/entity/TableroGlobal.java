8219127ddd71b0e0e1c2c125bf24a7165a875956
Neth Corrales <nethcordev343@gmail.com>
2026-02-17 17:46:13 -0400

    feat(TableroGlobal): implement next move routing logic with free-play fallback

--- tic-tac-toe/src/main/java/com/llajtarootvalley/tictactoe/model/entity/TableroGlobal.java
+++ tic-tac-toe/src/main/java/com/llajtarootvalley/tictactoe/model/entity/TableroGlobal.java
@@ -1,5 +1,34 @@
 package com.llajtarootvalley.tictactoe.model.entity;
 
 public class TableroGlobal {
+	private TableroLocal[][] tablerosLocales;
 
+    private int siguienteFilaTablero = -1;
+    private int siguienteColTablero = -1;
+
+    public TableroGlobal() {
+        tablerosLocales = new TableroLocal[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tablerosLocales[i][j] = new TableroLocal();
+            }
+        }
+    }
+
+    public TableroLocal obtenerTableroLocal(int fila, int col) {
+        return tablerosLocales[fila][col];
+    }
+    
+    public void actualizarProximoTableroObligatorio(int filaJugadaEnLocal, int colJugadaEnLocal) {
+        TableroLocal proximoTablero = tablerosLocales[filaJugadaEnLocal][colJugadaEnLocal];
+        
+        if (!proximoTablero.estaJugable()) {
+            this.siguienteFilaTablero = -1;
+            this.siguienteColTablero = -1;
+        } else {
+            this.siguienteFilaTablero = filaJugadaEnLocal;
+            this.siguienteColTablero = colJugadaEnLocal;
+        }
+    }
+    
 }
